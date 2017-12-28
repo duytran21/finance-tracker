75cd4b8ea1601d523371953de8b21cf20d90e904
duytran21 <alanduy21@yahoo.com>
2017-12-28 00:19:50 -0800

    Listing current user stock and adding removing stock function for current user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+
+  def can_add_stock?(ticker_symbol)
+  	under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
+
+  def under_stock_limit?
+  	(user_stocks.count < 10)
+  end
+
+  def stock_already_added?(ticker_symbol)
+  	# this function find if valid stock or not then go to check if it already existed in database then won't let user add to Portfolio.
+  	stock = Stock.find_by_ticker(ticker_symbol)
+  	return false unless stock
+  	user_stocks.where(stock_id: stock.id).exists? 
+  	# The line above means: for example: u = User.first => u.user_stocks.where(stock_id: 1).exists? return true if exists or false if doesn't
+  end
 end
