d969e9a2f8887a60dc882af272a9a7bcee95b3a7
duytran21 <alanduy21@yahoo.com>
2017-12-28 01:51:37 -0800

    Add first and last name to Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" } 
+
 
   resources :user_stocks, except: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
