b8572cc94205a776d33759c3d8130513ec0012ca
duytran21 <alanduy21@yahoo.com>
2017-12-28 02:08:38 -0800

    Fix adding more params first_name and last_name to Devise in new Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, :controllers => { :registrations => "user/registrations" } 
+  devise_for :users
 
 
   resources :user_stocks, except: [:show, :edit, :update]
