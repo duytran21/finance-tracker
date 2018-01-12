55a77a2898d2c362b024cdfce57134cd83e1b8f9
duytran21 <duytran211189@gmail.com>
2018-01-11 22:22:55 -0800

    Styling Home Slider

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module FinanceTracker
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 	config.assets.enabled = true
-	config.assets.paths << Rails.root.join('/app/assets/fonts') 
-	config.assets.paths << Rails.root.join('/app/assets/images')
+	config.assets.paths << Rails.root.join('/app/assets/fonts')
   end
 end
