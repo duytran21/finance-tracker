27dbabcd0a7ad0f3f263f7c9fd26f37e2b5e8f06
duytran21 <alanduy21@yahoo.com>
2018-02-09 23:59:03 -0800

    Styling Article Page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,6 +36,26 @@ var navbar_mobile = function() {
 	}
 }
 
+$(document).ready(function() {
+	$(window).scroll(function() {
+	    var top_of_element = $("#art-element-content").offset().top;
+	    var bottom_of_element = $("#art-element-content").offset().top + $("#art-element-content").outerHeight();
+	    var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+	    var top_of_screen = $(window).scrollTop();
+	    var bottom_of_fixed_element = $("#element-social-share").offset().top + $("#element-social-share").outerHeight();
+	    var top_of_fixed_element = $("#element-social-share").offset().top;
+	    if( top_of_screen >= top_of_element ){
+	    	$(".art-body-content").addClass("element-fixed");
+	    	if (bottom_of_fixed_element = bottom_of_element){
+	    		$(".art-body-content").removeClass("element-fixed");
+	    		$(".art-body-content").addClass("element-abs-bottom");
+	    	}
+	    }
+	    else {
+	        $(".art-body-content").removeClass("element-fixed");
+	    }
+	});
+});
 
 window.addEventListener('load', navbar_mobile, false);
 window.addEventListener('resize', navbar_mobile, false);
