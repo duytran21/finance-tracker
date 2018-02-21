5af4caf7a554892e1c1dee107edd04da9a32843e
duytran21 <alanduy21@yahoo.com>
2018-02-20 23:20:55 -0800

    Fixed not update Css correctly

--- public/assets/tinymce/plugins/hr/plugin.js
+++ public/assets/tinymce/plugins/hr/plugin.js
@@ -1,1 +1,1 @@
-!function(){"use strict";var n=function(n){n.addCommand("InsertHorizontalRule",function(){n.execCommand("mceInsertContent",!1,"<hr />")})},t=function(n){n.addButton("hr",{icon:"hr",tooltip:"Horizontal line",cmd:"InsertHorizontalRule"}),n.addMenuItem("hr",{icon:"hr",text:"Horizontal line",cmd:"InsertHorizontalRule",context:"insert"})};tinymce.util.Tools.resolve("tinymce.PluginManager").add("hr",function(o){n(o),t(o)})}();
+plugin-25fde4e2e7fa1a1d38c81ac00c74d9093cc83755e5f0577d5222f2d031c4ec23.js
