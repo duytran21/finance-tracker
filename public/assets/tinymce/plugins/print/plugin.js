2430723ccbe007719bdfc07179161763a28a72fe
duytran21 <alanduy21@yahoo.com>
2018-02-20 22:49:27 -0800

    Updated

--- public/assets/tinymce/plugins/print/plugin.js
+++ public/assets/tinymce/plugins/print/plugin.js
@@ -1,1 +1,1 @@
-plugin-984a7a3b4b52c24bb4b4ea8fc467228ff13c179e041c09a9b4ff30d971d646ab.js
+!function(){"use strict";var t=function(t){t.addCommand("mcePrint",function(){t.getWin().print()})},n=function(t){t.addButton("print",{title:"Print",cmd:"mcePrint"}),t.addMenuItem("print",{text:"Print",cmd:"mcePrint",icon:"print"})};tinymce.util.Tools.resolve("tinymce.PluginManager").add("print",function(i){t(i),n(i),i.addShortcut("Meta+P","","mcePrint")})}();
