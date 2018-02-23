a22fa5bfc59dd20e238974e95a1196a90a8a640f
duytran21 <duytran211189@gmail.com>
2018-02-22 18:52:00 -0800

    Update Navigation Menu

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:blogs, :show]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
