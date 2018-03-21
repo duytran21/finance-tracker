4f5f67ed91785aeecbdc52012c9cf721a5883362
duytran21 <alanduy21@yahoo.com>
2018-03-21 00:22:02 -0700

    Fix missing permit parameter in article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -75,7 +75,7 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :content, :description, :isVisible, :tags, :img_url)
+      params.require(:article).permit(:title, :content, :description, :isVisible, :tags, :img_url, :user_id)
     end
 
     def require_same_user
